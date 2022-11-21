f8a99235d4c422ca90b5274ec1d70848e1626754
Szymon Mrozicki <s.mrozicki123@gmail.com>
2022-11-21 16:25:10 +0100

    encounter function

--- organism.h
+++ organism.h
@@ -19,6 +19,8 @@ public:
     const species_t &get_species() {
         return species;
     }
+
+
 };
 
 template<typename species_t>
@@ -33,5 +35,11 @@ using Herbivore = Organism<species_t, false, true>;
 template<typename species_t>
 using Plant = Organism<species_t, false, false>;
 
+template <typename species_t, bool sp1_eats_m, bool sp1_eats_p, bool sp2_eats_m, bool sp2_eats_p>
+constexpr std::tuple<Organism<species_t, sp1_eats_m, sp1_eats_p>,
+                     Organism<species_t, sp2_eats_m, sp2_eats_p>,
+                     std::optional<Organism<species_t, sp1_eats_m, sp1_eats_p>>>
+encounter(Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
+          Organism<species_t, sp2_eats_m, sp2_eats_p> organism2);
 
 #endif 
